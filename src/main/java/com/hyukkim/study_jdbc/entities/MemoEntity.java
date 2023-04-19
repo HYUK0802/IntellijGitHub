63bea193c5593b72e2ceb90763592a37f351989e
김재혁 <hyuk@gimjaehyeog-ui-MacBookPro.local>
2023-04-20 02:12:12 +0900

    변경

--- src/main/java/com/hyukkim/study_jdbc/entities/MemoEntity.java
+++ src/main/java/com/hyukkim/study_jdbc/entities/MemoEntity.java
@@ -13,8 +13,8 @@ public class MemoEntity {
     }
 
     public MemoEntity setIndex(int index) {
-        this.index = index;
-        return this;
+        this.index = index; // 인스턴스 변수 index에 매개변수 index 값을 할당
+        return this;        // MemoEntity 인스턴스 자신을 반환
     }
 
     public Date getDate() {
@@ -35,16 +35,18 @@ public class MemoEntity {
         return this;
     }
 
+    // 객체의 index 값을 비교하여 같으면 true, 다르면 false를 반환하는 equals 메서드
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MemoEntity that = (MemoEntity) o;
-        return index == that.index;
+        if (this == o) return true; // 객체 자신과 인자로 받은 객체 o가 같은 객체라면 true 반환
+        if (o == null || getClass() != o.getClass()) return false; // o가 null이거나 클래스가 다르면 fasle 반환
+        MemoEntity that = (MemoEntity) o; // 형변환
+        return index == that.index; // 객체의 index 값이 같으면 true 반환, 다르면 false 반환
     }
 
+    // 객체의 index 값을 기반으로 hash 값을 반환하는 hashcode 메서드
     @Override
     public int hashCode() {
-        return Objects.hash(index);
+        return Objects.hash(index); // index 값을 기반으로 hash 값을 반환
     }
 }
--- src/main/java/com/hyukkim/study_jdbc/services/MemoService.java
+++ src/main/java/com/hyukkim/study_jdbc/services/MemoService.java
@@ -28,6 +28,13 @@ public class MemoService {
             return false;
 
         }
+        // memoDao의 insert() 메소드 호출 결과가 0보다 크면 메모 작성이 성공한 것이므로 true반환, 그렇지 않으면 false 반환
         return this.memoDao.insert(memo) > 0;
     }
 }
+//MemoService 클래스는 메모에 대한 CRUD(Create, Read, Update, Delete) 기능을 수행하기 위한 서비스 클래스이다.
+//private final MemoDao memoDao = new MemoDao(); : 서비스 클래스 내에서는 DAO 객체를 생성해 사용하며, 이를 위해 MemoDao 클래스를 필드로 갖는다.
+//public int getCount() throws Exception : 메모 전체 개수 조회를 위한 메소드이다. memoDao 객체의 selectCount() 메소드를 호출해 메모 전체 개수를 반환한다.
+//public boolean delete(int index) throws Exception : 메모 삭제를 위한 메소드이다. memoDao 객체의 delete(index) 메소드를 호출해 해당 인덱스의 메모를 삭제한다. 삭제된 row 수가 0보다 크면 삭제가 성공한 것으로 간주하고 true를 반환하고, 그렇지 않으면 false를 반환한다.
+//public List<MemoEntity> query(int page) throws Exception : 메모 조회를 위한 메소드이다. memoDao 객체의 select(page) 메소드를 호출해 해당 페이지에 있는 메모 리스트를 반환한다.
+//public boolean write(MemoEntity memo) throws Exception : 메모 작성을 위한 메소드이다. 메모 텍스트의 길이가 0글자 이상 1000글자 이하인지 확인하고, 조건에 맞지 않으면 false를 반환한다. memoDao 객체의 insert(memo) 메소드를 호출해 메모를 작성하고, 작성된 row 수가 0보다 크면 작성이 성공한 것으로 간주하고 true를 반환하고, 그렇지 않으면
